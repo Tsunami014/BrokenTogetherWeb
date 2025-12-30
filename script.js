9ba9816de4f0b0b09b45914175111ca7f9d85281
Tsunami014 <tsunami014@duck.com>
2025-12-30 13:47:26 +1100

    Made it print an entire screen

--- script.js
+++ script.js
@@ -4,10 +4,27 @@ const ctx = canvas.getContext('2d');
 
 const img = new Image();
 img.src = "assets/tile.svg"
+const img2 = new Image();
+img2.src = "assets/tile2.svg"
+
+function getTile(x, y) {
+    return (x+y)%2==0 ? img : img2
+}
 
 function draw() {
-    ctx.clearRect(0, 0, c.width, c.height); 
-    ctx.drawImage(img, 0, 0);
+    ctx.clearRect(0, 0, c.width, c.height);
+    const cols = 10
+    const twid = Math.ceil(c.width/cols)
+    const htwid = twid/2
+    const thei = Math.floor(twid/2)
+    const hthei = thei/2
+    const rows = Math.floor(c.height/thei)*2
+    for (let i = 0; i < rows+3; i++) {
+        for (let j = 0; j < cols+1; j++) {
+            const offs = i%2==0 ? 0 : htwid
+            ctx.drawImage(getTile(i, j), twid*j-offs, hthei*(i-1), twid, thei);
+        }
+    }
 }
 
 
@@ -19,7 +36,9 @@ function resizeCanvas() {
 window.addEventListener('resize', resizeCanvas);
 
 async function init() {
-    await img.decode() // Wait for image to be ready
+    // Wait for images to be ready
+    await img.decode()
+    await img2.decode()
     resizeCanvas(); // Initial draw
 }
 init()
